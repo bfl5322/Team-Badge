6e11213db434cbc3f9c0b0f8f91d3feb6de14cfa
bfl5322 <bfl5322@psu.edu>
2023-04-04 13:02:50 -0400

    yesyes

--- src/team-resource.js
+++ src/team-resource.js
@@ -18,7 +18,7 @@ class teamresource extends LitElement {
             display: block;
         }
         .wrapper {
-            
+            border: 1px solid black;
             display: inline-block;
             width: 40%;
             
@@ -39,15 +39,15 @@ constructor() {
     super();
     this.team = [
       {
-        "name": "Milwaukee Bucks",
-        "image": "https://upload.wikimedia.org/wikipedia/en/4/4a/Milwaukee_Bucks_logo.svg",
+        "name": "Amazon Cognito",
+        "image": "https://www.pikpng.com/transpng/bTowmb/",
         "location": "Milwaukee, WI",
         "wins": "53",
         "losses": "12",
         "Hof": "15" 
     },
     {
-        "name": "Philadelphia 76ers",
+        "name": "Amazon I AM",
         "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Philadelphia_76ers_Logo.svg/990px-Philadelphia_76ers_Logo.svg.png?20170916111445",
         "location": "Philadelphia, PA",
         "wins": "49",
@@ -55,7 +55,7 @@ constructor() {
         "Hof": "9" 
     },
     {
-        "name": "New York Knicks",
+        "name": "Signing for AWS Web Services",
         "image": "https://upload.wikimedia.org/wikipedia/en/2/25/New_York_Knicks_logo.svg",
         "location": "New York, NY",
         "wins": "42",
@@ -63,7 +63,7 @@ constructor() {
         "Hof": "22" 
     },
     {
-        "name": "Boston Celtics ",
+        "name": "AWS-LAMDA Phase 1",
         "image": "https://upload.wikimedia.org/wikipedia/en/8/8f/Boston_Celtics.svg",
         "location": "Boston, MA",
         "wins": "52",
@@ -71,7 +71,7 @@ constructor() {
         "Hof": "37" 
     },
     {
-        "name": "Brooklyn Nets",
+        "name": "Amazon S3 Phase 1",
         "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Brooklyn_Nets_newlogo.svg/1024px-Brooklyn_Nets_newlogo.svg.png",
         "location": "Brooklyn, Nets",
         "wins": "40",
@@ -83,13 +83,12 @@ constructor() {
 
   render() {
     return html`
-        <div class="wrapper">
+
             ${this.team.map(team => html`
             <div class="item">
                 <team-badge name="${team.name}" image="${team.image}" location="${team.location}" wins="${team.wins}" losses="${team.losses}" Hof="${team.Hof}"></team-badge>
             </div>
             `)}
-        </div>
     `;
   }
 }
